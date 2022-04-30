4afa0b2dc232829fc86b1ba8c70074522a960280
Shyngys Karishev <karishev@karishev.local>
2022-04-30 06:44:14 +0400

    style(footer): background color changed

--- src/components/templates/Footer.tsx
+++ src/components/templates/Footer.tsx
@@ -1,19 +1,38 @@
-import styles from '../UI/modules/Footer.module.css'
+import { useContext } from "react";
+import styles from "../UI/modules/Footer.module.css";
+import { ThemeContext } from "./Main";
+import { MainTheme } from "./MainTheme";
 
 export const Footer = () => {
-  const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear();
+  const { theme } = useContext(ThemeContext);
+  const headerStyle: MainTheme = {
+    dark: {
+      backgroundColor: "black",
+      color: "white",
+    },
+    light: {
+      backgroundColor: "lightgray",
+      color: "black",
+    },
+    common: {
+      transition: "all is ease",
+    },
+  };
 
+  const themeStyle = {
+    ...headerStyle.common,
+    ...(theme === "light" ? headerStyle.light : headerStyle.dark),
+  };
   return (
-    <footer className={styles.footer}>
+    <footer style={themeStyle} className={styles.footer}>
       <div className="container">
         <p className="text-muted">
           © {currentYear}, OneLab. All rights reserved
         </p>
         <p></p>
-        <p className={styles.created}>
-          Created with love by Ali and Shyngys
-        </p>
+        <p className={styles.created}>Created with love by Ali and Shyngys</p>
       </div>
     </footer>
-  )
-}
+  );
+};

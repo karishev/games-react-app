2b164b750a595eb19e9d867d08a7562662a1a8db
Shyngys Karishev <karishev@karishev.local>
2022-04-30 16:13:12 +0400

    style(aboutPage): changed the margin

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -1,30 +1,30 @@
-import React, { useContext } from 'react'
-import Container from '@mui/material/Container'
-import styles from '../UI/modules/About.module.css'
-import ava1 from '../assets/1.jpg'
-import ava2 from '../assets/2.jpg'
-import { ThemeContext } from '../templates/Main'
-import { MainTheme } from '../templates/MainTheme'
+import React, { useContext } from "react";
+import Container from "@mui/material/Container";
+import styles from "../UI/modules/About.module.css";
+import ava1 from "../assets/1.jpg";
+import ava2 from "../assets/2.jpg";
+import { ThemeContext } from "../templates/Main";
+import { MainTheme } from "../templates/MainTheme";
 export const AboutPage = () => {
-  const { theme } = useContext(ThemeContext)
+  const { theme } = useContext(ThemeContext);
   const headerStyle: MainTheme = {
     dark: {
-      backgroundColor: '#161515ed',
-      color: 'white',
+      backgroundColor: "#161515ed",
+      color: "white",
     },
     light: {
-      backgroundColor: '#f9f8f8',
-      color: 'black',
+      backgroundColor: "#f9f8f8",
+      color: "black",
     },
     common: {
-      transition: 'all 1s ease',
+      transition: "all 1s ease",
     },
-  }
+  };
 
   const themeStyle = {
     ...headerStyle.common,
-    ...(theme === 'light' ? headerStyle.light : headerStyle.dark),
-  }
+    ...(theme === "light" ? headerStyle.light : headerStyle.dark),
+  };
   return (
     <Container maxWidth="sm">
       <section className={styles.about__section} style={themeStyle}>
@@ -35,41 +35,39 @@ export const AboutPage = () => {
           way.
         </p>
       </section>
-      <h2 style={{ textAlign: 'center' }}>Our Team</h2>
+      <h2 style={{ textAlign: "center" }}>Our Team</h2>
       <div className={styles.row}>
         <div className={styles.column}>
           <div className={styles.card}>
-            <img src={ava1} alt="" style={{ width: '200px' }} />
+            <img src={ava1} alt="" style={{ width: "200px" }} />
             <div className={styles.container}>
               <h2>Shyngys</h2>
               <p className={styles.tittle}>Front-End Developer</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>mike@example.com</p>
+              <p>kvrishev@gmail.com</p>
               <p>
-                <button className={styles.button}>
-                  <a href="https://github.com/karishev">Contact</a>
-                </button>
+                <a href="https://github.com/karishev">
+                  <button className={styles.button}>Contact</button>
+                </a>
               </p>
             </div>
           </div>
         </div>
         <div className={styles.column}>
           <div className={styles.card}>
-            <img src={ava2} alt="" style={{ width: '200px' }} />
+            <img src={ava2} alt="" style={{ width: "200px" }} />
             <div className={styles.container}>
               <h2>Ali</h2>
               <p className={styles.tittle}>Front-End Developer</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>mike@example.com</p>
+              <p>alichok_krasavchik@mail.ru</p>
               <p>
-                <button className={styles.button}>
-                  <a href="https://github.com/ZuGo06">Contact</a>
-                </button>
+                <a href="https://github.com/ZuGo06">
+                  <button className={styles.button}>Contact</button>
+                </a>
               </p>
             </div>
           </div>
         </div>
       </div>
     </Container>
-  )
-}
+  );
+};
